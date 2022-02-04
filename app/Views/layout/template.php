cd40d9a328401fa55780e6497e2b2e2296d78081
Maria Dimansi Chrysandini <chrysan.scorpio@gmail.com>
2022-02-04 18:42:19 +0700

    split navbar to its own layout

--- app/Views/layout/template.php
+++ app/Views/layout/template.php
@@ -12,21 +12,8 @@
 
 <body>
 
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Hey Not!</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div class="navbar-nav">
-                    <a class="nav-link active" aria-current="page" href="<?= base_url('/'); ?>">Home</a>
-                    <a class="nav-link" href="<?= base_url('pages/about'); ?>">About</a>
-                    <a class="nav-link" href="<?= base_url('pages/contact'); ?>">Contact</a>
-                </div>
-            </div>
-        </div>
-    </nav>
+    <!-- navbar here -->
+    <?= $this->include('layout/navbar'); ?>
 
     <!-- put your content here -->
     <?= $this->renderSection('content'); ?>
