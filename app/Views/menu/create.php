3aa0db4d18fde8eae70cbe587e412c254376f1bd
Maria Dimansi Chrysandini <chrysan.scorpio@gmail.com>
2022-02-14 23:55:33 +0700

    resolve error on create button and adding some form field

--- app/Views/menu/create.php
+++ app/Views/menu/create.php
@@ -7,41 +7,70 @@
             <h2 class="col-8">Add Menu Form</h2>
             <form>
                 <div class="row mb-3">
-                    <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
+                    <label for="inputEmail3" class="col-sm-2 col-form-label">Menu Name</label>
                     <div class="col-sm-10">
                         <input type="email" class="form-control" id="inputEmail3">
                     </div>
                 </div>
                 <div class="row mb-3">
-                    <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
+                    <label for="inputPassword3" class="col-sm-2 col-form-label">Description</label>
                     <div class="col-sm-10">
                         <input type="password" class="form-control" id="inputPassword3">
                     </div>
                 </div>
                 <fieldset class="row mb-3">
-                    <legend class="col-form-label col-sm-2 pt-0">Radios</legend>
+                    <legend class="col-form-label col-sm-2 pt-0">Category</legend>
                     <div class="col-sm-10">
                         <div class="form-check">
                             <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios1" value="option1" checked>
                             <label class="form-check-label" for="gridRadios1">
-                                First radio
+                                Indomie
                             </label>
                         </div>
                         <div class="form-check">
                             <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios2" value="option2">
                             <label class="form-check-label" for="gridRadios2">
-                                Second radio
+                                Roti Bakar
                             </label>
                         </div>
                         <div class="form-check disabled">
-                            <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios3" value="option3" disabled>
+                            <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios3" value="option3">
                             <label class="form-check-label" for="gridRadios3">
-                                Third disabled radio
+                                Cemilan
                             </label>
                         </div>
                     </div>
                 </fieldset>
-                <div class="row mb-3">
+                <fieldset class="row mb-3">
+                    <legend class="col-form-label col-sm-2 pt-0">Subcategory</legend>
+                    <div class="col-sm-10">
+                        <div class="form-check">
+                            <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios1" value="option1" checked>
+                            <label class="form-check-label" for="gridRadios1">
+                                Goreng
+                            </label>
+                        </div>
+                        <div class="form-check">
+                            <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios2" value="option2">
+                            <label class="form-check-label" for="gridRadios2">
+                                Kuah
+                            </label>
+                        </div>
+                        <div class="form-check disabled">
+                            <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios3" value="option3">
+                            <label class="form-check-label" for="gridRadios3">
+                                Gorengan
+                            </label>
+                        </div>
+                        <div class="form-check disabled">
+                            <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios3" value="option3">
+                            <label class="form-check-label" for="gridRadios3">
+                                Bakaran
+                            </label>
+                        </div>
+                    </div>
+                </fieldset>
+                <!-- <div class="row mb-3">
                     <div class="col-sm-10 offset-sm-2">
                         <div class="form-check">
                             <input class="form-check-input" type="checkbox" id="gridCheck1">
@@ -50,7 +79,7 @@
                             </label>
                         </div>
                     </div>
-                </div>
+                </div> -->
                 <button type="submit" class="btn btn-primary">Sign in</button>
             </form>
         </div>
