357d103f34e1b3a0b8f3870778e7ed705edbe125
Maria Dimansi Chrysandini <chrysan.scorpio@gmail.com>
2022-02-26 23:47:23 +0700

    adding carousel to home

--- app/Views/pages/home.php
+++ app/Views/pages/home.php
@@ -1,12 +1,38 @@
 <?= $this->extend('layout/template'); ?>
 
 <?= $this->section('content'); ?>
-<div class="container">
-    <div class="row">
-        <div class="col">
-            <h1>Ini home!</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam deleniti hic dolor nisi, animi facilis itaque eos porro. Minus provident quae eaque esse. Sint, commodi quas cum veritatis adipisci quos.</p>
+<div class="container-fluid px-0">
+    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
         </div>
+        <div class="carousel-inner">
+            <div class="carousel-item">
+                <div class="fill">
+                    <img src="https://images.unsplash.com/photo-1588001291548-948f55922bfd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=844&q=80" class="d-block w-100" alt="noddles in white bowl">
+                </div>
+            </div>
+            <div class="carousel-item">
+                <div class="fill">
+                    <img src="https://images.unsplash.com/photo-1626509653291-5558b258f107?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" class="d-block w-100" alt="noodles in red bowl">
+                </div>
+            </div>
+            <div class="carousel-item active">
+                <div class="fill">
+                    <img src="https://images.unsplash.com/photo-1574756358916-ebdae0a5a437?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=848&q=80" class="d-block w-100" alt="noodles smoke">
+                </div>
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
     </div>
 </div>
 <?= $this->endSection(); ?>
