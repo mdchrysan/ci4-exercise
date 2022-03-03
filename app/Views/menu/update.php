84138603ee06338fdabbab01cf9f800a5f58979a
Maria Dimansi Chrysandini <chrysan.scorpio@gmail.com>
2022-03-03 23:37:48 +0700

    auto filled edit form

--- app/Views/menu/update.php
+++ app/Views/menu/update.php
@@ -12,7 +12,7 @@
                     <label for="menuName" class="col-sm-2 col-form-label">Menu Name</label>
                     <div class="col-sm-10">
                         <!-- ternary operation for is-invalid class -->
-                        <input type="text" class="form-control <?= ($validation->hasError('name')) ? 'is-invalid' : ''; ?>" id="menuName" name="menuName" value="<?= old('name'); ?>" autofocus>
+                        <input type="text" class="form-control <?= ($validation->hasError('name')) ? 'is-invalid' : ''; ?>" id="menuName" name="menuName" value="<?= $menu['name']; ?>" autofocus>
                         <div class="invalid-feedback">
                             <?= $validation->getError('name'); ?>
                         </div>
@@ -21,14 +21,14 @@
                 <div class="row mb-3">
                     <label for="description" class="col-sm-2 col-form-label">Description</label>
                     <div class="col-sm-10">
-                        <input type="text" class="form-control" id="description" name="description" value="<?= old('description'); ?>">
+                        <input type="text" class="form-control" id="description" name="description" value="<?= $menu['description']; ?>">
                     </div>
                 </div>
                 <div class="row mb-3">
                     <label for="price" class="col-sm-2 col-form-label">Price</label>
                     <div class="col-sm-10">
                         <!-- ternary operation for is-invalid class -->
-                        <input type="number" class="form-control <?= ($validation->hasError('price')) ? 'is-invalid' : ''; ?>" id="price" name="price" placeholder="15000" value="<?= old('price'); ?>">
+                        <input type="number" class="form-control <?= ($validation->hasError('price')) ? 'is-invalid' : ''; ?>" id="price" name="price" placeholder="15000" value="<?= $menu['price']; ?>">
                         <div class="invalid-feedback">
                             <?= $validation->getError('price'); ?>
                         </div>
@@ -37,7 +37,7 @@
                 <div class="row mb-3">
                     <label for="image" class="col-sm-2 col-form-label">Image</label>
                     <div class="col-sm-10">
-                        <input type="text" class="form-control" id="image" name="image" placeholder="food.PNG" value="<?= old('image'); ?>">
+                        <input type="text" class="form-control" id="image" name="image" placeholder="food.PNG" value="<?= $menu['image']; ?>">
                     </div>
                 </div>
                 <fieldset class="row mb-3">
