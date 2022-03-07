71240d114f08c91165a0d8148a76aa2aafd1feda
Maria Dimansi Chrysandini <chrysan.scorpio@gmail.com>
2022-03-07 23:59:39 +0700

    edit about

--- app/Views/pages/about.php
+++ app/Views/pages/about.php
@@ -4,8 +4,10 @@
 <div class="container">
     <div class="row">
         <div class="col">
-            <h1>Ini about yang layoutnya dipisah!</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam deleniti hic dolor nisi, animi facilis itaque eos porro. Minus provident quae eaque esse. Sint, commodi quas cum veritatis adipisci quos.</p>
+            <div class="row mt-5">
+                <h1>Apa itu Warmindo?</h1>
+                <p>Warung Indomie Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi voluptas beatae quibusdam molestias consequuntur autem, fugiat rerum qui, impedit dignissimos sapiente nemo odio? Quae, quod doloribus? Illo reprehenderit non itaque!</p>
+            </div>
         </div>
     </div>
 </div>
