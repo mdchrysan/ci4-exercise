917550f41c427dc4d9c0945beea61b103b4c7a42
Maria Dimansi Chrysandini <chrysan.scorpio@gmail.com>
2022-03-11 23:57:23 +0700

    create location model and controller

---
+++ app/Controllers/Branch.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\BranchModel;
+
+class Branch extends BaseController
+{
+    // use property
+    protected $branchModel;
+
+    //makes the model's called alongside with its class, so all the method can use it
+    public function __construct()
+    {
+        $this->branchModel = new BranchModel();
+    }
+
+    public function index()
+    {
+        $branch = $this->branchModel->getBranch();
+
+        // d($this->request->getVar('keyword'));
+        $keyword = $this->request->getVar('keyword');
+        if ($keyword) {
+            $this->branchModel->search($keyword);
+        } else {
+            $branch;
+        }
+
+        $data = [
+            'title' => 'Location | MyExercise',
+            'branch' => $this->branchModel->findAll()
+        ];
+
+        return view('pages/location', $data);
+    }
+}
---
+++ app/Models/BranchModel.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class BranchModel extends Model
+{
+    protected $table      = 'location';
+    protected $primaryKey = 'id';
+    protected $allowedFields = ['branch_name', 'address'];
+    protected $useTimestamps = true;
+
+    public function search($keyword)
+    {
+        // query builder class
+        $builder = $this->table('menu');
+        $builder->like('branch_name', $keyword);
+        return $builder;
+        // return $this->table('menu')->like('branch_name', $keyword)->orLike('city', $keyword);
+    }
+}
