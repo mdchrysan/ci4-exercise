9b2519b006ebaa8ba390fd9e2d648700b1ea0537
Maria Dimansi Chrysandini <chrysan.scorpio@gmail.com>
2022-03-13 01:36:55 +0700

    testing random data on location with faker

--- app/Database/Seeds/LocationSeeder.php
+++ app/Database/Seeds/LocationSeeder.php
@@ -9,30 +9,42 @@ class LocationSeeder extends Seeder
 {
     public function run()
     {
-        $data = [
-            [
-                'branch_name'   => 'Warmindo Pak Eko',
-                'address'       => 'Kenangan Street No.28',
-                'city'          => 'Yogyakarta',
-                'created_at'    => Time::now(),
-                'updated_at'    => Time::now()
-            ],
-            [
-                'branch_name'   => 'Warmindo Abang Adek',
-                'address'       => 'Kemerdekaan Street No.17',
-                'city'          => 'Jakarta',
-                'created_at'    => Time::now(),
-                'updated_at'    => Time::now()
-            ]
-        ];
+        // $data = [
+        //     [
+        //         'branch_name'   => 'Warmindo Pak Eko',
+        //         'address'       => 'Jl. Kenangan No.28',
+        //         'city'          => 'Yogyakarta',
+        //         'created_at'    => Time::now(),
+        //         'updated_at'    => Time::now()
+        //     ],
+        //     [
+        //         'branch_name'   => 'Warmindo Abang Adek',
+        //         'address'       => 'Jl. Kemerdekaan No.17',
+        //         'city'          => 'Jakarta',
+        //         'created_at'    => Time::now(),
+        //         'updated_at'    => Time::now()
+        //     ]
+        // ];
 
-        // Simple Queries
+        // Using Simple Queries
         // $this->db->query("INSERT INTO location (branch_name, address, city, created_at, updated_at) VALUES(:branch_name:, :address:, :city:, :created_at:, :updated_at:)", $data);
 
-        // Using Query Builder
-        // insert data only works for one data
-        // $this->db->table('location')->insert($data);
-        // insertBatch works for array
-        $this->db->table('location')->insertBatch($data);
+        // Using Query Builder insertBatch -> works for array
+        // $this->db->table('location')->insertBatch($data);
+
+        $faker = \Faker\Factory::create('id_ID');
+
+        for ($i = 0; $i < 10; $i++) {
+            $data = [
+                'branch_name'   => 'Warmindo' . ' ' . $faker->firstName . ' ' . $faker->lastName,
+                'address'       => $faker->streetAddress,
+                'city'          => $faker->city,
+                'created_at'    => $faker->dateTime()->format('Y-m-d H:i:s'),
+                'updated_at'    => Time::now()
+            ];
+
+            // Using Query Builder insert -> only works for one group of data
+            $this->db->table('location')->insert($data);
+        }
     }
 }
