64c010a4eabd4ba3a42ba881130717dc8562935e
Maria Dimansi Chrysandini <chrysan.scorpio@gmail.com>
2022-03-18 21:00:59 +0700

    edit Email, Validation, and failed Filters Config

--- app/Config/Email.php
+++ app/Config/Email.php
@@ -9,12 +9,12 @@ class Email extends BaseConfig
     /**
      * @var string
      */
-    public $fromEmail;
+    public $fromEmail = 'accouthen@gmail.com';
 
     /**
      * @var string
      */
-    public $fromName;
+    public $fromName = 'Warmindo Authenticator';
 
     /**
      * @var string
