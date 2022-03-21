2f4c1bffdba97e6f81d3a60135b95d63da40af18
Maria Dimansi Chrysandini <chrysan.scorpio@gmail.com>
2022-03-21 23:52:26 +0700

    Email config

--- app/Views/layout/navbar.php
+++ app/Views/layout/navbar.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
         <a class="navbar-brand" href="#">Warmindo</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
@@ -10,7 +10,7 @@
                 <a class="nav-link" href="<?= base_url('/pages/about'); ?>">About</a>
                 <a class="nav-link" href="<?= base_url('/branch'); ?>">Location</a>
                 <a class="nav-link" href="<?= base_url('/menu'); ?>">Menu</a>
-                <a class="nav-link pull-right" href="<?= base_url('/'); ?>">Login</a>
+                <a class="nav-link" href="<?= base_url('/'); ?>">Login</a>
             </div>
         </div>
     </div>
